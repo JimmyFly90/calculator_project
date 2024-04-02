328ff9cb7ae8b5e4ed9a21e02c83eda796986fca
JimmyFly90 <137488857+JimmyFly90@users.noreply.github.com>
2024-04-02 11:39:43 +0100

    Add files via upload

--- Calculator-Project-new.js
+++ Calculator-Project-new.js
@@ -2,51 +2,33 @@
 // C will clear screen to show zero and be ready for next equation
 // back button ← to remove last element on screen
 // convert button output on screen to be numbers and arithmetic symbols
+// remove zero from screen so it doens't preceed numbers
 // equal to show answer to math equation
 
-let screen = document.querySelector(".screen");
+const screen = document.querySelector(".screen")
 
 function clear() {
   screen.innerHTML = '0';
 }
 
-function calculate() {
-  return screen.innerHTML;
+function enterValue(event) {
+  const value = (event.target.innerHTML);
+  if (screen.innerHTML == '0') {
+    // I converted this to a string because the if statement is checking for a string
+    screen.innerHTML = value.toString();
+    } else {
+    screen.innerHTML += value;
+    } 
 }
 
 function backButton() {
-  screen.innerHTML += screen.innerHTML.slice(0, -1);
+  screen.innerHTML = (screen.innerHTML.slice(0, -1));
 }
 
-function enterSymbol(event) {
-  screen.innerHTML = (event.target.innerHTML);
+function calculate() {
+  screen.innerHTML = eval(screen.innerHTML);
 }
+// eval is converting strings to numbers
 
-function enterNumber(event) {
-  const value = parseInt((event.target.innerHTML), 10);
-
-  /* your code originally was this
-
-   screen.innerHTML +=  parseInt((event.target.innerHTML), 10);
-
-   plus and equals will take the current value and add the new value to it
-
-   So what you want to do is check if the current value is 0, if it is then you want to replace it with the new value
-
-   If it is not 0 then you want to add the new value to the current value
-  */
-
-  if (screen.innerHTML === '0') {
-    // I converted this to a string because the if statement is checking for a string
-    screen.innerHTML = value.toString();
-  } else {
-    screen.innerHTML += value;
-  }
-
-}
-
-document.getElementById("calc-button-double").addEventListener("click", clear, false);
-document.querySelector(".calc-button-row").addEventListener("click", enterNumber, false);
-document.querySelector("#symbol-button").addEventListener("click", enterSymbol, false);
-document.getElementById("back-button").addEventListener("click", backButton, false);
-document.getElementById("result").addEventListener("click", calculate, false);
+document.getElementById("calc-button-double").addEventListener("click", clear);
+document.getElementById("calc-buttons");
